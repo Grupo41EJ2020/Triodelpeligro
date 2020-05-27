fafb2237eca6bcb76ff2dd49d64f1bc92f25aa34
luiksrey <luiks13@hotmail.com>
2020-05-27 14:04:41 -0500

    stored procedure curso y empleado

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -86,3 +86,104 @@ BEGIN
 	WHERE IdTema = @IdTema
 END;
 go
+
+CREATE PROCEDURE sp_Empleado_ConsultarTodo
+AS
+BEGIN
+	SELECT * FROM Empleado
+END;
+go
+
+CREATE PROCEDURE sp_Empleado_Insertar
+	@IdEmpleado int,
+	@Nombre nvarchar(100),
+	@Direccion nvarchar(100)
+AS
+BEGIN
+	INSERT INTO Empleado (IdEmpleado,Nombre,Direccion)
+	VALUES (@IdEmpleado,@Nombre,@Direccion)
+END;
+print('--Script Finalizado--')
+go
+
+CREATE PROCEDURE sp_Empleado_ConsultarPorID
+@IdEmpleado int
+AS
+BEGIN
+	SELECT * FROM Empleado
+	WHERE IdEmpleado = @IdEmpleado
+END;
+go
+GO
+CREATE PROCEDURE sp_Empleado_Eliminar
+	@IdEmpleado int
+AS
+BEGIN
+	DELETE FROM Empleado
+	WHERE IdEmpleado = @IdEmpleado
+END;
+go
+GO
+CREATE PROCEDURE sp_Empleado_Actualizar
+	@IdEmpleado int,
+	@Nombre nvarchar(100),
+	@Direccion nvarchar(100)
+AS
+BEGIN
+	UPDATE Empleado SET Nombre=@Nombre, Direccion=@Direccion
+	WHERE IdEmpleado = @IdEmpleado
+END;
+print('--Script Finalizado--')
+go
+
+
+
+CREATE PROCEDURE sp_Curso_ConsultarTodo
+AS
+BEGIN
+	SELECT * FROM Curso
+END;
+go
+
+CREATE PROCEDURE sp_Curso_ConsultarPorID
+@IdCurso int
+AS
+BEGIN
+	SELECT * FROM Curso
+	WHERE IdCurso = @IdCurso
+END;
+go
+GO
+
+CREATE PROCEDURE sp_Curso_Insertar
+	@IdCurso int,
+	@Descripcion nvarchar(200),
+	@IdEmpleado nvarchar(100)
+AS
+BEGIN
+	INSERT INTO Curso (IdCurso,Descripcion,IdEmpleado)
+	VALUES (@IdCurso,@Descripcion,@IdEmpleado)
+END;
+print('--Script Finalizado--')
+go
+CREATE PROCEDURE sp_Curso_Eliminar
+	@IdCurso int
+AS
+BEGIN
+	DELETE FROM Curso
+	WHERE IdCurso = @IdCurso
+END;
+go
+GO
+CREATE PROCEDURE sp_Curso_Actualizar
+	@IdCurso int,
+	@Descripcion nvarchar(200),
+	@IdEmpleado nvarchar(100)
+AS
+BEGIN
+	UPDATE Curso SET Descripcion=@Descripcion, IdEmpleado=@IdEmpleado
+	WHERE IdCurso= @IdCurso
+END;
+print('--Script Finalizado--')
+go
+
